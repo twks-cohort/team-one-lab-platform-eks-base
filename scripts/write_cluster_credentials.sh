5d83ff5c9e3fad8420c7d7a8604cd44846576941
Sheneeka Ward <sheneeka.ward@thoughtworks.com>
2023-10-05 20:17:15 -0400

    [Sheneeka] (Module_4) Setup up eks-base pipeline

--- scripts/write_cluster_credentials.sh
+++ scripts/write_cluster_credentials.sh
@@ -11,14 +11,15 @@ export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAcces
 export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
 
 # terraform-aws-eks module v17 method
-cat kubeconfig_$CLUSTER | opw write platform-${CLUSTER} kubeconfig -
+cat kubeconfig_$CLUSTER | opw write team-one-platform-${CLUSTER} kubeconfig -
 
 # terraform-aws-eks module v18 method
 # terraform output kubeconfig | grep -v "EOT" | opw write platform-${CLUSTER} kubeconfig -
 
 # write cluster url and pubic certificate to 1password
-terraform output cluster_endpoint | tr -d \\n | sed 's/"//g' | opw write platform-${CLUSTER} cluster-endpoint -
-terraform output cluster_certificate_authority_data | tr -d \\n | sed 's/"//g' | opw write platform-${CLUSTER} base64-certificate-authority-data -
+# team-one-platform-nonprod-us-east-2
+terraform output cluster_endpoint | tr -d \\n | sed 's/"//g' | opw write team-one-platform-${CLUSTER} cluster-endpoint -
+terraform output cluster_certificate_authority_data | tr -d \\n | sed 's/"//g' | opw write team-one-platform-${CLUSTER} base64-certificate-authority-data -
 
 
 # platform-sandbox-us-east-2
